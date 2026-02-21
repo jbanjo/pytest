b68f274347650178661ea7c4a2f700205c1dcf19
JohannesSchmidt <johannes.schmidt369@gmail.com>
2026-02-21 16:20:25 +0100

    Changed file paths again.

--- tests/test_notebook_outputs.py
+++ tests/test_notebook_outputs.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
 def test_output_exists():
-    repoRootDirectory = Path(__file__).resolve().parent
+    repoRootDirectory = Path(__file__).resolve().parent.parent
     testingDirectory = repoRootDirectory / "notebooks/outputs"
     assert (testingDirectory / "sample_output.txt").exists(), "Sample Output is Missing!"
 
 def test_output_correct(testing_directory, comparison_directory):
-    repoRootDirectory = Path(__file__).resolve().parent
+    repoRootDirectory = Path(__file__).resolve().parent.parent
     testingDirectory = repoRootDirectory / "notebooks/outputs"
     comparisonDirectory = repoRootDirectory / "tests/comparison_outputs"
     assert (testingDirectory / "sample_output.txt").read_text() == (comparisonDirectory / "sample_output.txt").read_text(), "Sample Output not correct!"
